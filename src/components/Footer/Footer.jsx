40ec022b50d5e055d8bc273609c30c244de6a11b
Saket281 <131553738+Saket281@users.noreply.github.com>
2024-04-18 03:23:04 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { Link } from "react-router-dom"; // Import Link component
+import { Link } from "react-router-dom"; 
 import "./footer.css";
 import { CartContext } from "../../CartContext";
 const Footer = () => {
